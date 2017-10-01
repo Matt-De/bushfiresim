f119d874267a5232e3855bcd9601561958c5b4c9
Matt-De <mpdem1@student.monash.edu>
2017-10-01 19:28:55 +1100

    Grid spacing issue fixed. Now draws squares.

--- public/javascripts/simulator.js
+++ public/javascripts/simulator.js
@@ -1,8 +1,15 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+/* draws a square of specified colour (hex code) */
+function drawSquare(x, y, colour) {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle=colour;
+    ctx.fillRect(x, y, 5, 5);
+}
 
 /* draws a square grid */
 function drawGrid() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
     for (var x = 0.5; x <= 300.5; x += 5) {
       ctx.moveTo(x, 0);
       ctx.lineTo(x, 300.5);
@@ -13,7 +20,11 @@ function drawGrid() {
     }
     ctx.stroke();
 }
-
-
+    
+for(var i=0; i<60; i++) {
+    for(var j=0; j<60; j++) {
+        drawSquare(j*5, i*5, "#FFFFFF");
+    }
+} 
 
 drawGrid();
